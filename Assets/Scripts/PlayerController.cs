24c62f8dc6b87fbbebca32595904e121d49db79d
Hex0Lio <biological256.ai@gmail.com>
2022-07-25 22:52:17 +0300

    rigidbody-player-control
    - Added full functionality for a rigidbody player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,31 +5,35 @@ public class PlayerController : MonoBehaviour
     public Transform playerObj;
     Rigidbody playerRb;
 
-    Vector3 moveDir;
-    float hInput;
-    float vInput;
-    bool isGrounded;
-    bool readyToJump = true;
-
     [Header("Movement")]
     public float speed;
     public float groundDrag;
+    Vector3 moveDir;
+    float hInput;
+    float vInput;
 
     [Header("Ground Check")]
     public LayerMask groundMask;
     public float checkRadius;
     public Transform checkPos;
+    bool isGrounded;
 
     [Header("Jumping")]
     public float jumpHeight;
     public float jumpCooldown;
     public float airMultiplier;
+    bool readyToJump = true;
 
     [Header("Step Climb")]
     public Transform lowerRay;
     public Transform upperRay;
     public float stepHeight;
     public float stepSmooth;
+
+    [Header("Slope Climb")]
+    public float maxClimbAngle;
+    RaycastHit slopeHit;
+    bool exitingSlope;
 
     // Start is called before the first frame update
     void Start()
@@ -43,13 +47,17 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         DoGroundCheck();
+
         InputListener();
+        
+        AddGroundDrag();
+        ControlSpeed();
     }
 
     void FixedUpdate()
     {
-        //StepClimb();
         Move();
+        StepClimb();
     }
 
     void InputListener()
@@ -64,10 +72,7 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    void DoGroundCheck()
-    {
-        isGrounded = Physics.CheckSphere(checkPos.position, checkRadius, groundMask);
-    }
+    void DoGroundCheck() => isGrounded = Physics.CheckSphere(checkPos.position, checkRadius, groundMask);
 
     void AddGroundDrag()
     {
@@ -76,13 +81,18 @@ public class PlayerController : MonoBehaviour
         else
             playerRb.drag = 0f;
     }
-
     void ControlSpeed()
     {
-        Vector3 flatVel = new Vector3(playerRb.velocity.x, 0, playerRb.velocity.z);
-        if (flatVel.magnitude > speed) {
-            Vector3 limitedVel = flatVel.normalized * speed;
-            playerRb.velocity = new Vector3(limitedVel.x, playerRb.velocity.y, limitedVel.z);
+        if (IsOnSlope() && !exitingSlope) {
+            if (playerRb.velocity.magnitude > speed) {
+                playerRb.velocity = playerRb.velocity.normalized * speed;
+            }
+        } else {
+            Vector3 flatVel = new Vector3(playerRb.velocity.x, 0, playerRb.velocity.z);
+            if (flatVel.magnitude > speed) {
+                Vector3 limitedVel = flatVel.normalized * speed;
+                playerRb.velocity = new Vector3(limitedVel.x, playerRb.velocity.y, limitedVel.z);
+            }
         }
     }
 
@@ -90,49 +100,67 @@ public class PlayerController : MonoBehaviour
     {
         moveDir = (playerObj.right * hInput + playerObj.forward * vInput).normalized;
 
+        // On slope
+        if (isGrounded && IsOnSlope() && !exitingSlope) {
+            playerRb.AddForce(speed * 10f * GetSlopeMoveDir(), ForceMode.Force);
+
+            if (playerRb.velocity.y > 0) playerRb.AddForce(Vector3.down * 10f, ForceMode.Force);
+        }
         // On ground
-        if (isGrounded)
-            playerRb.MovePosition(transform.position + speed * Time.fixedDeltaTime * moveDir);
-        //playerRb.AddForce(speed * 10f * moveDir, ForceMode.Force);
-        //transform.Translate(speed * Time.deltaTime * moveDir, Space.World);
+        else if (isGrounded)
+            playerRb.AddForce(speed * 10f * moveDir, ForceMode.Force);
         // In the air
         else
-            playerRb.MovePosition(transform.position + speed * airMultiplier * Time.fixedDeltaTime * moveDir);
-        //playerRb.AddForce(speed * airMultiplier * 10f * moveDir, ForceMode.Force);
-        //transform.Translate(speed * airMultiplier * Time.deltaTime * moveDir, Space.World);
+            playerRb.AddForce(speed * airMultiplier * 10f * moveDir, ForceMode.Force);
+
+        playerRb.useGravity = !IsOnSlope();
     }
 
     void Jump()
     {
-        float jumpForce = Mathf.Sqrt(-2 * Physics.gravity.y * jumpHeight);
+        exitingSlope = true;
+
+        float jumpForce = Mathf.Sqrt(-2 * Physics.gravity.y * jumpHeight) * 1.3f;
         playerRb.velocity = new Vector3(playerRb.velocity.x, jumpForce, playerRb.velocity.z);
     }
-    void ResetJump() => readyToJump = true;
+    void ResetJump()
+    {
+        exitingSlope = false;
+        readyToJump = true;
+    }
 
     void StepClimb()
     {
-        //RaycastHit hitLower;
-        if (Physics.Raycast(lowerRay.position, moveDir, 0.2f)) {
-            //RaycastHit hitUpper;
-            if (!Physics.Raycast(upperRay.position, moveDir, 0.3f)) {
-                playerRb.position -= new Vector3(0, -stepSmooth * Time.deltaTime, 0);
+        Vector3[] directions = {
+            moveDir,
+            Quaternion.AngleAxis(45, Vector3.up) * moveDir,
+            Quaternion.AngleAxis(-45, Vector3.up) * moveDir
+        };
+
+        foreach (Vector3 dir in directions) {
+            RaycastHit hitLower;
+            if (Physics.Raycast(lowerRay.position, dir, out hitLower, 0.75f)) {
+                float angle = Vector3.Angle(Vector3.up, hitLower.normal);
+                if (!Physics.Raycast(upperRay.position, dir, 1) && angle == 90) {
+                    playerRb.position -= new Vector3(0, -stepSmooth * Time.deltaTime, 0);
+                }
+            }
+        }
+    }
+
+    bool IsOnSlope()
+    {
+        if (Physics.SphereCast(transform.position, 0.5f, Vector3.down, out slopeHit, 1)) {
+            float angle = Vector3.Angle(Vector3.up, slopeHit.normal);
+            if (angle <= maxClimbAngle && angle != 0) {
+                return true;
             }
         }
 
-        //RaycastHit hitLowerLeft;
-        if (Physics.Raycast(lowerRay.position, Quaternion.AngleAxis(45, Vector3.up) * moveDir, 0.2f)) {
-            //RaycastHit hitUpperLeft;
-            if (!Physics.Raycast(upperRay.position, Quaternion.AngleAxis(45, Vector3.up) * moveDir, 0.3f)) {
-                playerRb.position -= new Vector3(0, -stepSmooth * Time.deltaTime, 0);
-            }
-        }
-
-        //RaycastHit hitLowerMinusRight;
-        if (Physics.Raycast(lowerRay.position, Quaternion.AngleAxis(-45, Vector3.up) * moveDir, 0.2f)) {
-            //RaycastHit hitUpperRight;
-            if (!Physics.Raycast(upperRay.position, Quaternion.AngleAxis(-45, Vector3.up) * moveDir, 0.3f)) {
-                playerRb.position -= new Vector3(0, -stepSmooth * Time.deltaTime, 0);
-            }
-        }
+        return false;
+    }
+    Vector3 GetSlopeMoveDir()
+    {
+        return Vector3.ProjectOnPlane(moveDir, slopeHit.normal).normalized;
     }
 }
